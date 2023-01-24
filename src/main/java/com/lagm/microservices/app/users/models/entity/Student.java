cfba04f8ac909295ffb6643c7e0a0f10a067aad1
Luis Guisado <lguisadom@gmail.com>
2023-01-24 00:09:50 -0500

    Adding Student entity class

---
+++ src/main/java/com/lagm/microservices/app/users/models/entity/Student.java
@@ -0,0 +1,67 @@
+package com.lagm.microservices.app.users.models.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private String firstName;
+    private String email;
+
+    @Column(name="create_at")
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
