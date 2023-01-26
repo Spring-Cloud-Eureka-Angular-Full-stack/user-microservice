c332a446d5593d51ceb37f478a551569e64029ec
Luis Guisado <lguisadom@gmail.com>
2023-01-26 00:44:56 -0500

    feat: add eureka cliente

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO students(create_at, email, firstname, lastname) VALUES ('2023-01-23', 'felipelopez@gmail.com', 'Felipe', 'Lopez');
+INSERT INTO students(create_at, email, firstname, lastname) VALUES ('2023-01-23', 'juangonzales@gmail.com', 'Juan', 'Gonzales');
+INSERT INTO students(create_at, email, firstname, lastname) VALUES ('2023-01-23', 'carlosbarraza@gmail.com', 'Carlos', 'Barraza');
+INSERT INTO students(create_at, email, firstname, lastname) VALUES ('2023-01-23', 'carolinaherrera@gmail.com', 'Carolina', 'Herrera');
